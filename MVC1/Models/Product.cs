646e99ded9e72bc7fd853f90a65a008b8765fbb4
FREDCOM-PC <FREDCOM-PC@FREDCOM-PC>
2018-03-25 01:04:12 -0400

    Cambiar campo ID del modelo Product por ProductID

--- MVC1/Models/Product.cs
+++ MVC1/Models/Product.cs
@@ -9,7 +9,7 @@ namespace MVC1.Models
     public class Product
     {
         [Key]
-        public int Id { get; set; }
+        public int ProductID { get; set; }
         public string Description { get; set; }
         public int Price { get; set; }
         public DateTime LastBuy { get; set; }
